1f5538f249b49f2592c683e49f9d11f86e160815
Alessandro Cruz <62727627+AlessandroCruz01@users.noreply.github.com>
2022-05-05 11:31:57 -0500

    add scrollbar e forms

--- web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -15,23 +15,85 @@ export function FeedbackContentStep({feedbackType , onFeedbackRestartRequested} 
     return(
         <>
             <header>
-                <button className="top-5 left-5 absolute text-zinc-400 hover:text-zinc-100" type="button" onClick={onFeedbackRestartRequested}>
+
+                <button 
+                    className="
+                        top-5 
+                        left-5 
+                        absolute 
+                        text-zinc-400 
+                        hover:text-zinc-100
+                    " 
+                    type="button" 
+                    onClick={onFeedbackRestartRequested}
+                >
+
                     <ArrowLeft weight="bold" className="w-4 h-4"/>
+
                 </button>
      
                 <span className="text-xl leading-6 flex items-center gap-2">
-                    <img src={feedbackTypeInfo.image.source} alt={feedbackTypeInfo.image.alt} className="w-6 h-6"/>
+
+                    <img 
+                        src={feedbackTypeInfo.image.source} 
+                        alt={feedbackTypeInfo.image.alt} 
+                        className="w-6 h-6"
+                    />
+
                     {feedbackTypeInfo.title}
+
                 </span>
 
                 <CloseButton />
             </header>
-            <div className="flex py-8 gap-2 w-full">
-          
 
-                
-                
-            </div>
+            <form className="my-4 w-full">
+
+                <textarea 
+                    className="
+                        min-w[304px]
+                        w-full
+                        min-h-[112px]
+                        text-sm 
+                        placeholder-zinc-400 
+                        border-zinc-600 
+                        bg-transparent 
+                        rounded-md 
+                        focus:border-brand-500 
+                        focus:ring-brand-500
+                        focus:ring-1
+                        resize-none
+                        focus:outline-none
+                        scrollbar-thumb-zinc-400
+                        scrollbar-thin
+                    " 
+                    placeholder="Conte com detalhes o que esta acontecendo..."
+                >
+                </textarea>
+
+                <footer>
+                    <button
+                        type="submit"
+                        className="
+                            p-2
+                            bg-brand-500
+                            rounded-md
+                            border-transparent
+                            flex-1 flex 
+                            justify-center 
+                            items-center
+                            text-sm
+                            hover:bg-brand-300
+                        "   
+                    >
+
+                        Enviar feedback
+
+                    </button>
+                </footer>
+            </form>
+
+
         </>
     );
 }
