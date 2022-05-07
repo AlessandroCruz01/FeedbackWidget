e861c057e2cb8f8fcec7c3fa785f06b2188ca6e1
Alessandro Cruz <alessandrocruzadm01@gmail.com>
2022-05-07 14:16:57 -0500

    finalizado o envio de email

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,7 @@
 //SQlite
 import express from "express";
 import { prisma } from "./prisma";
+import nodemailer from "nodemailer";
 
 
 const app = express();
@@ -18,6 +19,29 @@ app.post("/feedbacks", async (req, res) => {
             screenshot
         }
     });
+
+    const transport = nodemailer.createTransport({
+        host: "smtp.mailtrap.io",
+        port: 2525,
+        auth: {
+            user: "143427e47e60b5",
+            pass: "8b81e8817b403f"
+        }
+    });
+    
+    await transport.sendMail({
+        from: "Equipe Feedget <oi@feedget.com>",
+        to: "Alessandro Cruz <alessandrocruzadm01@gmail.com>",
+        subject: "Novo feedback",
+        html: [
+            "<div style=\"font-family: sans-serif\">",
+            `<p>Tipo do feedbakc: ${type}</p>`,
+            `<p>Comentario: ${comment}</p>`,
+            "</div>",
+        ].join("\n")
+    
+    });
+    
 
     return res.status(201).json({data: feedback});
 } );
