c592eb52f1c11a97a8c05c2e523dc3834949b801
Alessandro Cruz <alessandrocruzadm01@gmail.com>
2022-05-06 19:45:07 -0500

    config prisma, criacao do model feedback e dando start nas mighrations

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,13 @@
+//SQlite
 import express from "express";
 
 const app = express();
 
+app.get("/", (req, res) => {
+    res.json({message: "Server exec"});
+} );
+
 app.listen(3333, () => {
     console.log("backend Exec");
 });
+
