593985c2c30f9c3602988be4014f7f7bbc278fde
Alessandro Cruz <alessandrocruzadm01@gmail.com>
2022-05-06 20:00:27 -0500

    rota de cadastro de feedback

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,25 @@
 //SQlite
 import express from "express";
+import { prisma } from "./prisma";
+
 
 const app = express();
+app.use(express.json()); //Middleware
 
-app.get("/", (req, res) => {
-    res.json({message: "Server exec"});
+
+
+app.post("/feedbacks", async (req, res) => {
+    const { type, comment, screenshot } = req.body;
+
+    const feedback = await prisma.feedback.create({
+        data: {
+            type,
+            comment,
+            screenshot
+        }
+    });
+
+    return res.status(201).json({data: feedback});
 } );
 
 app.listen(3333, () => {
