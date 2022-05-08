21f66eefe58c45431b26f1812187d66caca2a6ef
Alessandro Cruz <alessandrocruzadm01@gmail.com>
2022-05-07 23:25:54 -0500

    adicionando testes unitarios

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,6 +1,6 @@
 import express from "express";
 // import nodemailer from "nodemailer";
-import { SubmitFeedbackUseCase } from "./use-cases/submit-feedbakc-use-case";
+import { SubmitFeedbackUseCase } from "./use-cases/submit-feedback-use-case";
 import { PrismaFeedbacksRepository } from "./repositories/prisma/prisma-feedbakc-repository";
 import { NodemailerMailAdapter } from "./adapters/nodemailer/nodemailer-mail-adapter";
 
