ce36de0f0b9e1622df1a61aca13a4fb1942d371d
Alessandro Cruz <alessandrocruzadm01@gmail.com>
2022-05-08 16:31:35 -0500

    finish mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import "react-native-gesture-handler";
+
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaView } from "react-native";
+
+import { Widget } from "./src/components/Widget";
+
+import AppLoading from "expo-app-loading";
+import { useFonts, Inter_400Regular, Inter_500Medium } from "@expo-google-fonts/inter";
+
+import { theme } from "./src/theme";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    const [fontsLoaded] = useFonts({
+        Inter_400Regular,
+        Inter_500Medium
+    });
+
+    if (!fontsLoaded) {
+        return <AppLoading />;
+    }
+
+    return (
+        <SafeAreaView style={{
+            flex: 1,
+            backgroundColor: theme.colors.background
+        }}>
+
+
+            <StatusBar 
+                style="light" 
+                backgroundColor="transparent" 
+                translucent 
+            />
+
+            
+            <Widget />W
+        </SafeAreaView>
+    );
+}
